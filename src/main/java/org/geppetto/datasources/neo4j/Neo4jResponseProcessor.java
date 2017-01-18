6614e9dbb7e33238ab15b0680de1f53834975f32
Rob Court <robbie.court@gmail.com>
2017-01-18 18:05:14 +0000

    handling blank list

--- src/main/java/org/geppetto/datasources/neo4j/Neo4jResponseProcessor.java
+++ src/main/java/org/geppetto/datasources/neo4j/Neo4jResponseProcessor.java
@@ -27,22 +27,26 @@ public class Neo4jResponseProcessor implements IQueryResponseProcessor
 	public QueryResults processResponse(Map<String, Object> response)
 	{
 		QueryResults results = DatasourcesFactory.eINSTANCE.createQueryResults();
-		List<String> headers = (List<String>) ((List) ((Map<String, Object>) ((List) response.get("results")).get(0)).get("columns"));
+		if (response.get("results")).size() > 0){
+			List<String> headers = (List<String>) ((List) ((Map<String, Object>) ((List) response.get("results")).get(0)).get("columns"));
 
-		results.getHeader().addAll(headers);
+			results.getHeader().addAll(headers);
 
-		results.getResults().clear();
-		List<Map<String, Object>> data = (List<Map<String, Object>>) ((List) ((Map<String, Object>) ((List) response.get("results")).get(0)).get("data"));
-		for(Map<String, Object> rowObject : data)
-		{
-			QueryResult resultRow = DatasourcesFactory.eINSTANCE.createQueryResult();
-			List<Object> row = (List<Object>) rowObject.get("row");
-			for(Object value : row)
+			results.getResults().clear();
+			List<Map<String, Object>> data = (List<Map<String, Object>>) ((List) ((Map<String, Object>) ((List) response.get("results")).get(0)).get("data"));
+			for(Map<String, Object> rowObject : data)
 			{
-				resultRow.getValues().add(value);
+				QueryResult resultRow = DatasourcesFactory.eINSTANCE.createQueryResult();
+				List<Object> row = (List<Object>) rowObject.get("row");
+				for(Object value : row)
+				{
+					resultRow.getValues().add(value);
+				}
+				results.getResults().add(resultRow);
 			}
-			results.getResults().add(resultRow);
-		}
+		}else{
+			//TODO: report reponse to log
+		}	
 		return results;
 	}
 
