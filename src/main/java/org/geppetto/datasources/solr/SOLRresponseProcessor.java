10d27d7dbea87ae7a527304c44d51205d911062e
jrmartin <yeshuarmartinez@gmail.com>
2023-02-15 17:02:21 -0800

    Fixes to solr query processor

--- src/main/java/org/geppetto/datasources/solr/SOLRresponseProcessor.java
+++ src/main/java/org/geppetto/datasources/solr/SOLRresponseProcessor.java
@@ -4,15 +4,23 @@
 package org.geppetto.datasources.solr;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.geppetto.datasources.IQueryResponseProcessor;
 import org.geppetto.model.datasources.DatasourcesFactory;
 import org.geppetto.model.datasources.QueryResult;
 import org.geppetto.model.datasources.QueryResults;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * @author Robbie1977
@@ -34,12 +42,19 @@ public class SOLRresponseProcessor implements IQueryResponseProcessor
 		double numFound =  (double)data.get("numFound");
 		if(numFound > 0)
 		{
-			Set<String> headers = new HashSet<String>();
+			List<String> headers = new ArrayList<String>();
 			String queryName = null;
 			results.getResults().clear();
 			List<Object> rowObject = (List<Object>) data.get("docs");
 			Object terminfo = rowObject.get(0);
-
+			Gson gsonObj = new Gson();
+			String jsonStr = gsonObj.toJson(terminfo);
+			JsonParser parser = new JsonParser();
+			JsonObject jsonFormat = (JsonObject) parser.parse(jsonStr);
+			JsonArray termInfo = (JsonArray) jsonFormat.get("term_info");
+			String json = termInfo.get(0).getAsString();
+			jsonFormat = (JsonObject) parser.parse(json);
+			
 			QueryResult resultRow = DatasourcesFactory.eINSTANCE.createQueryResult();
 			if (queryName == null) {
 				Set<String> keySet = data.keySet();
@@ -48,9 +63,14 @@ public class SOLRresponseProcessor implements IQueryResponseProcessor
 				queryName = keySet.toString();
 			}
 			if (headers.size() < 1) {
-				headers.add("JSON");
+				Set<Entry<String, JsonElement>> keySet = jsonFormat.entrySet();	
+				
+				for (Entry<String, JsonElement> entry : keySet) {
+					headers.add(entry.getKey());
+					JsonElement obj = entry.getValue();
+					resultRow.getValues().add(obj);
+				}
 			}
-			resultRow.getValues().add(terminfo);
 			results.getResults().add(resultRow);
 			results.getHeader().addAll(headers);
 		}
