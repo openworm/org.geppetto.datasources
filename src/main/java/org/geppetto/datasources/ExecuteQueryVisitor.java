b69e9dfa474a188551c3e13452c54d989114fadb
Rob Court <robbie.court@gmail.com>
2025-03-21 19:40:55 +0000

    Add import for Map.Entry in ExecuteQueryVisitor

--- src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
+++ src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
@@ -3,6 +3,7 @@ package org.geppetto.datasources;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
