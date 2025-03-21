1bd14c2130ad67dcb36d83345162dd553e18f595
Rob Court <robbie.court@gmail.com>
2025-03-21 20:42:49 +0000

    Remove unused processingOutputMap from ExecuteQueryVisitor

--- src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
+++ src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
@@ -48,8 +48,6 @@ public class ExecuteQueryVisitor extends DatasourcesSwitch<Object>
 
 	private Variable variable;
 
-	private Map<String, Object> processingOutputMap = new HashMap<String, Object>();
-
 	private GeppettoModelAccess geppettoModelAccess;
 
 	private int resultsCount = -1;
