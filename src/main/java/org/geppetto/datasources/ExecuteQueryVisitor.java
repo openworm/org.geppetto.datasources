9ff4954f3ce5de474380a5b7de958cdaee9b02bd
Rob Court <robbie.court@gmail.com>
2025-03-22 12:31:40 +0000

    Simplify processing output map check and enhance logging in ExecuteQueryVisitor

--- src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
+++ src/main/java/org/geppetto/datasources/ExecuteQueryVisitor.java
@@ -186,22 +186,10 @@ public class ExecuteQueryVisitor extends DatasourcesSwitch<Object>
 
                     // Check if processing output map is empty
                     if (processingOutputMap.isEmpty()) {
-                        try {
-                            IQueryProcessor queryProcessor = (IQueryProcessor) ServiceCreator.getNewServiceInstance(query.getQueryProcessorId());
-                            this.results = queryProcessor.process(query, getDataSource(query), getVariable(), getResults(), geppettoModelAccess);
-                            System.out.println("Calling query processor: " + queryProcessor.getClass().getName());
-                            this.processingOutputMap = queryProcessor.getProcessingOutputMap();
-                            for (Entry<String, Object> entry : processingOutputMap.entrySet()) {
-                                System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
-                            }
-                        } catch(GeppettoInitializationException e) {
-                            return new GeppettoVisitingException(e);
-                        } catch(GeppettoDataSourceException e) {
-                            return new GeppettoVisitingException(e);
-                        }
+                        System.out.println("SimpleQuery: " + query.getName());
                     }
 
-					if(processingOutputMap != null)
+					if(processingOutputMap != null && !processingOutputMap.isEmpty())
 					{
 						System.out.println("Processing output map: " + processingOutputMap);
 						properties.putAll(processingOutputMap);
