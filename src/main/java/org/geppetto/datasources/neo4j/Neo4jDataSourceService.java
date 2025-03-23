f180596d1827afdf0a31d8fb45ad6274c274517d
Rob Court <robbie.court@gmail.com>
2025-03-23 04:31:29 +0000

    Revert "Add imports for query processing and update VSCode settings for null analysis"
    This reverts commit 669d6f96aa4566e7da85c1aa52c498fd2ec94b02.

--- src/main/java/org/geppetto/datasources/neo4j/Neo4jDataSourceService.java
+++ src/main/java/org/geppetto/datasources/neo4j/Neo4jDataSourceService.java
@@ -1,17 +1,8 @@
 
 package org.geppetto.datasources.neo4j;
 
-import org.geppetto.core.datasources.GeppettoDataSourceException;
-import org.geppetto.core.datasources.IQueryListener;
 import org.geppetto.datasources.ADataSourceService;
-import org.geppetto.datasources.ExecuteQueryVisitor;
 import org.geppetto.datasources.IQueryResponseProcessor;
-import org.geppetto.model.datasources.Query;
-import org.geppetto.model.datasources.QueryResults;
-import org.geppetto.model.util.GeppettoModelTraversal;
-import org.geppetto.model.util.GeppettoVisitingException;
-import org.geppetto.model.variables.Variable;
-import org.geppetto.model.variables.VariablesFactory;
 
 /**
  * @author matteocantarelli
